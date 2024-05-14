d833aed0f75046c449f960b38c7ad142a96cc636
zsoltikaa <100851329+zsoltikaa@users.noreply.github.com>
2024-05-14 22:12:28 +0200

    Update filter.js

--- JavaScript/Autopia/js/filter.js
+++ JavaScript/Autopia/js/filter.js
@@ -11,3 +11,20 @@ function toggleFilters() {
         filterMenu.style.display = "none";
     }
 }
+
+function toggleFilters() {
+    const filterMenu = document.querySelector('.filterMenu');
+    
+    if (filterMenu.classList.contains('active')) {
+        filterMenu.classList.remove('animate__fadeInDown');
+        filterMenu.classList.add('animate__fadeOutUp');
+
+        filterMenu.addEventListener('animationend', function handleAnimationEnd() {
+            filterMenu.classList.remove('active', 'animate__fadeOutUp');
+            filterMenu.removeEventListener('animationend', handleAnimationEnd);
+        });
+    } else {
+        filterMenu.classList.remove('animate__fadeOutUp');
+        filterMenu.classList.add('active', 'animate__fadeInDown');
+    }
+}
